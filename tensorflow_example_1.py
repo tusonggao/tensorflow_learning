b8f4fd56558f24455f554deeb1af50576a5a422b
tusonggao <tusonggao@163.com>
2020-01-15 10:14:39 +0800

    update

--- tensorflow_example_1.py
+++ tensorflow_example_1.py
@@ -1,4 +1,8 @@
 # https://v1.tf.wiki/zh/basic.html
+import os
+import sys
+import pandas as pd
+import numpy as np
 import tensorflow as tf
 tf.enable_eager_execution()
 
@@ -20,4 +24,10 @@ print('zero_vector is ', zero_vector)
 A = tf.constant([[1., 2.], [3., 4.]])
 B = tf.constant([[5., 6.], [7., 8.]])
 print('A.shape is ', A.shape)
+
+X_raw = np.array([2013, 2014, 2015, 2016, 2017], dtype=np.float32)
+y_raw = np.array([12000, 14000, 15000, 16500, 17500], dtype=np.float32)
+
+X = (X_raw - X_raw.min()) / (X_raw.max() - X_raw.min())
+y = (y_raw - y_raw.min()) / (y_raw.max() - y_raw.min())
 
