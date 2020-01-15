d543c18984342415c44c2c61cfb6a8c205e26ef4
tusonggao <tusonggao@163.com>
2020-01-15 11:02:53 +0800

    update

--- tensorflow_example_1.py
+++ tensorflow_example_1.py
@@ -45,5 +45,27 @@ for e in range(num_epoch):
     # 更新参数
     a, b = a - learning_rate * grad_a, b - learning_rate * grad_b
 
-print(a, b)
+print('with numpy a, b is ', a, b)
+
+##########################################################################################
+
+X = tf.constant(X)
+y = tf.constant(y)
+
+a = tf.get_variable('a', dtype=tf.float32, shape=[], initializer=tf.zeros_initializer)
+b = tf.get_variable('b', dtype=tf.float32, shape=[], initializer=tf.zeros_initializer)
+variables = [a, b]
+
+num_epoch = 10000
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-3)
+for e in range(num_epoch):
+    # 使用tf.GradientTape()记录损失函数的梯度信息
+    with tf.GradientTape() as tape:
+        y_pred = a * X + b
+        loss = 0.5 * tf.reduce_sum(tf.square(y_pred - y))
+    # TensorFlow自动计算损失函数关于自变量（模型参数）的梯度
+    grads = tape.gradient(loss, variables)
+    # TensorFlow自动根据梯度更新参数
+    optimizer.apply_gradients(grads_and_vars=zip(grads, variables))
+print('a is ', a, 'b is ', b)
 
